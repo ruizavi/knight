7cbd1f7a7b9f066212c9edd3b6518028ed0f4172
avi <avi@ruiz.com>
2024-02-08 09:23:53 -0600

    fix: change definition of stories, using individual components and use render propertie

--- src/stories/Callout.stories.tsx
+++ src/stories/Callout.stories.tsx
@@ -1,10 +1,30 @@
 import { Meta, StoryObj } from "@storybook/react";
-import { Callout } from "..";
+import {
+  Callout,
+  CalloutHeader,
+  CalloutIcon,
+  CalloutMessage,
+  CalloutTitle,
+} from "..";
 import { SVG } from "../components/icons/link";
 
 const meta = {
   title: "Callout",
   component: Callout,
+  render: (args) => (
+    <Callout {...args}>
+      <CalloutHeader>
+        <CalloutIcon>
+          <SVG />
+        </CalloutIcon>
+        <CalloutTitle>Information</CalloutTitle>
+      </CalloutHeader>
+      <CalloutMessage>
+        Neque porro quisquam est qui dolorem ipsum quia dolor sit
+        amet,consectetur, adipisci velit...
+      </CalloutMessage>
+    </Callout>
+  ),
   parameters: {
     layout: "centered",
   },
@@ -15,104 +35,34 @@ const meta = {
 } satisfies Meta<typeof Callout>;
 
 export default meta;
-type Story = StoryObj<typeof meta>;
+type Story = StoryObj<typeof Callout>;
 
 export const Info: Story = {
   args: {
     variant: "info",
-    children: (
-      <>
-        <Callout.Header>
-          <Callout.Icon>
-            <SVG />
-          </Callout.Icon>
-          <Callout.Title>Information</Callout.Title>
-        </Callout.Header>
-        <Callout.Message>
-          Neque porro quisquam est qui dolorem ipsum quia dolor sit
-          amet,consectetur, adipisci velit...
-        </Callout.Message>
-      </>
-    ),
   },
 };
 
 export const Success: Story = {
   args: {
     variant: "success",
-    children: (
-      <>
-        <Callout.Header>
-          <Callout.Icon>
-            <SVG />
-          </Callout.Icon>
-          <Callout.Title>Success</Callout.Title>
-        </Callout.Header>
-        <Callout.Message>
-          Neque porro quisquam est qui dolorem ipsum quia dolor sit
-          amet,consectetur, adipisci velit...
-        </Callout.Message>
-      </>
-    ),
   },
 };
 
 export const Warning: Story = {
   args: {
     variant: "warning",
-    children: (
-      <>
-        <Callout.Header>
-          <Callout.Icon>
-            <SVG />
-          </Callout.Icon>
-          <Callout.Title>Warning</Callout.Title>
-        </Callout.Header>
-        <Callout.Message>
-          Neque porro quisquam est qui dolorem ipsum quia dolor sit
-          amet,consectetur, adipisci velit...
-        </Callout.Message>
-      </>
-    ),
   },
 };
 
 export const Error: Story = {
   args: {
     variant: "error",
-    children: (
-      <>
-        <Callout.Header>
-          <Callout.Icon>
-            <SVG />
-          </Callout.Icon>
-          <Callout.Title>Error</Callout.Title>
-        </Callout.Header>
-        <Callout.Message>
-          Neque porro quisquam est qui dolorem ipsum quia dolor sit
-          amet,consectetur, adipisci velit...
-        </Callout.Message>
-      </>
-    ),
   },
 };
 
 export const Themeless: Story = {
   args: {
     variant: "themeless",
-    children: (
-      <>
-        <Callout.Header>
-          <Callout.Icon>
-            <SVG />
-          </Callout.Icon>
-          <Callout.Title>Themeless</Callout.Title>
-        </Callout.Header>
-        <Callout.Message>
-          Neque porro quisquam est qui dolorem ipsum quia dolor sit
-          amet,consectetur, adipisci velit...
-        </Callout.Message>
-      </>
-    ),
   },
 };
--- src/stories/Card.stories.tsx
+++ src/stories/Card.stories.tsx
@@ -1,42 +1,51 @@
 import { Meta, StoryObj } from "@storybook/react";
 import { Card } from "..";
+import {
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+  CardFooter,
+} from "../components/card";
 
 const meta = {
   title: "Card",
   component: Card,
+  render: (args) => (
+    <Card {...args}>
+      <CardHeader>
+        <CardTitle>Title</CardTitle>
+        <CardDescription>Lorem ipsum dolor sit amet.</CardDescription>
+      </CardHeader>
+      <CardContent>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas, animi
+        eum, ipsum ipsam pariatur perferendis ut provident consequatur earum ab
+        inventore! Magnam earum cum voluptates.
+      </CardContent>
+      <CardFooter>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat,
+        nisi.
+      </CardFooter>
+    </Card>
+  ),
   parameters: {
     layout: "centered",
   },
   tags: ["autodocs"],
   argTypes: {
-    shadow: { control: "select" },
+    shadow: {
+      control: "select",
+    },
   },
 } satisfies Meta<typeof Card>;
 
 export default meta;
-type Story = StoryObj<typeof meta>;
+type Story = StoryObj<typeof Card>;
 
 export const None: Story = {
   args: {
     shadow: "none",
     className: "w-[350px]",
-    children: (
-      <>
-        <Card.Header>
-          <Card.Title>Title</Card.Title>
-          <Card.Description>Lorem ipsum dolor sit amet.</Card.Description>
-        </Card.Header>
-        <Card.Content>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas, animi
-          eum, ipsum ipsam pariatur perferendis ut provident consequatur earum
-          ab inventore! Magnam earum cum voluptates.
-        </Card.Content>
-        <Card.Footer>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat,
-          nisi.
-        </Card.Footer>
-      </>
-    ),
   },
 };
 
@@ -44,23 +53,6 @@ export const Ligth: Story = {
   args: {
     shadow: "ligth",
     className: "w-[350px]",
-    children: (
-      <>
-        <Card.Header>
-          <Card.Title>Title</Card.Title>
-          <Card.Description>Lorem ipsum dolor sit amet.</Card.Description>
-        </Card.Header>
-        <Card.Content>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas, animi
-          eum, ipsum ipsam pariatur perferendis ut provident consequatur earum
-          ab inventore! Magnam earum cum voluptates.
-        </Card.Content>
-        <Card.Footer>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat,
-          nisi.
-        </Card.Footer>
-      </>
-    ),
   },
 };
 
@@ -68,23 +60,6 @@ export const Medium: Story = {
   args: {
     shadow: "medium",
     className: "w-[350px]",
-    children: (
-      <>
-        <Card.Header>
-          <Card.Title>Title</Card.Title>
-          <Card.Description>Lorem ipsum dolor sit amet.</Card.Description>
-        </Card.Header>
-        <Card.Content>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas, animi
-          eum, ipsum ipsam pariatur perferendis ut provident consequatur earum
-          ab inventore! Magnam earum cum voluptates.
-        </Card.Content>
-        <Card.Footer>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat,
-          nisi.
-        </Card.Footer>
-      </>
-    ),
   },
 };
 
@@ -92,22 +67,5 @@ export const Hard: Story = {
   args: {
     shadow: "hard",
     className: "w-[350px]",
-    children: (
-      <>
-        <Card.Header>
-          <Card.Title>Title</Card.Title>
-          <Card.Description>Lorem ipsum dolor sit amet.</Card.Description>
-        </Card.Header>
-        <Card.Content>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas, animi
-          eum, ipsum ipsam pariatur perferendis ut provident consequatur earum
-          ab inventore! Magnam earum cum voluptates.
-        </Card.Content>
-        <Card.Footer>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat,
-          nisi.
-        </Card.Footer>
-      </>
-    ),
   },
 };
